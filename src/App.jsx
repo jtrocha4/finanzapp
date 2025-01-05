500c546d5c52fa5b4dcaa6384c5ab9a03fa90fdd
jtrocha4 <jtrocha04@gmail.com>
2025-01-04 21:04:12 -0500

    update App and index

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import './App.css'
-import ModalFixedExpenses from './components/ModalFixedExpenses'
 import TableFixedExpenses from './components/TableFixedExpenses'
 import Widget from './components/widget'
+import { ExpensesContext } from './contexts/ExpensesContext'
 
 function App () {
-  const [salary, setSalary] = useState(1300)
-  const [netSalary, setNetSalary] = useState(0)
-  const [fixedExpenses, setFixedExpenses] = useState([])
-
-  // const expenses = [{
-  //   id: 1,
-  //   description: 'Transporte',
-  //   value: 90
-  // }]
-
-  const calculateNetSalary = (salary, expenses) => {
-    let totalExpenses = 0
-
-    expenses.map(({ value }) => {
-      totalExpenses = totalExpenses + value
-    })
-    console.log(totalExpenses)
-    const netSalary = salary - totalExpenses
-    setNetSalary(netSalary)
-  }
-
-  useEffect(() => {
-    calculateNetSalary(salary, fixedExpenses)
-  }, [fixedExpenses])
+  const { expenses, setExpenses } = useContext(ExpensesContext)
 
   return (
     <>
       <section>
-        <Widget description='Sueldo' value={salary} />
-        <Widget description='Sueldo Neto' value={netSalary} />
+        <Widget description='Sueldo' value='1300000' />
+        <Widget description='Sueldo Neto' value='' />
       </section>
       <section>
         <h2>Gastos fijos</h2>
-        <ModalFixedExpenses setExpenses={setFixedExpenses} expenses={fixedExpenses} />
-        <TableFixedExpenses tableName='Gastos Fijos' expenses={fixedExpenses} />
+        <TableFixedExpenses expenses={expenses} setExpenses={setExpenses} />
       </section>
     </>
   )
