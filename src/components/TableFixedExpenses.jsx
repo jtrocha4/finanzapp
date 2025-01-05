4a7647008abfdb1fdf710cdc0f33cc2e148451b2
jtrocha4 <jtrocha04@gmail.com>
2025-01-04 21:05:22 -0500

    update(components): TableFixedExpenses

--- src/components/TableFixedExpenses.jsx
+++ src/components/TableFixedExpenses.jsx
@@ -1,25 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ModalFixedExpenses from './ModalFixedExpenses'
 
-const TableFixedExpenses = ({ tableName, expenses }) => {
+const TableFixedExpenses = ({ expenses, setExpenses }) => {
+  const [edit, setEdit] = useState(false)
+  const [expenseToEdit, setExpenseToEdit] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const deleteExpensive = (expenseId) => {
+    const result = expenses.filter(({ id }) => id !== expenseId)
+    setExpenses(result)
+  }
+
+  const getExpensive = (expenseId) => {
+    const result = expenses.find(({ id }) => id === expenseId)
+    setEdit(!edit)
+    setExpenseToEdit(result)
+  }
+
+  const calculateExpenses = (expenses) => {
+    let total = 0
+    expenses.forEach(({ value }) => {
+      setTotal(total += Number(value))
+    })
+
+    console.log(total)
+  }
+
+  useEffect(() => {
+    calculateExpenses(expenses)
+  }, [expenses])
+
   return (
     <>
-      <h3>{tableName}</h3>
+      <ModalFixedExpenses expenses={expenses} setExpenses={setExpenses} edit={edit} setEdit={setEdit} expenseToEdit={expenseToEdit} />
+      <h3>Gastos fijos</h3>
       <table>
         <thead>
           <tr>
             <th>Descripcion</th>
+            <th>Fecha</th>
             <th>Valor</th>
+            <th>Acciones</th>
           </tr>
         </thead>
         <tbody>
           {
-          expenses.map(({ key, description, value }) => (
-            <tr key={key}>
-              <td>{description}</td>
-              <td>{value}</td>
-            </tr>
-          ))
-        }
+            expenses.map(({ id, date, description, value }) => (
+              <tr key={id}>
+                <td>{description}</td>
+                <td>{date}</td>
+                <td>{value}</td>
+                <td>
+                  <button onClick={() => getExpensive(id)}>Editar</button>
+                  <button onClick={() => deleteExpensive(id)}>Eliminar</button>
+                </td>
+              </tr>
+            ))
+          }
+          <tr>
+            <td colSpan={2}>
+              Total
+            </td>
+            <td>{total}</td>
+          </tr>
         </tbody>
       </table>
     </>
